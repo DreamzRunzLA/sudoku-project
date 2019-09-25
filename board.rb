970add19d1eb0aa229f1e4c694e028a93d6e707a
DreamzRunzLA <salmaan.a.qadir@gmail.com>
2019-09-24 22:09:33 -0600

    got the from_file function working

--- board.rb
+++ board.rb
@@ -3,11 +3,17 @@ require_relative 'tile'
 class Board
 
     def initialize(grid)
-
+        @grid = grid
     end
 
     def from_file
-        
+        arr = File.readlines('puzzles/sudoku1.txt').map do |line|
+            line.split.map do |ele|
+                ele.to_s
+                ele.split('')
+            end
+        end
+        return arr.map { |ele| ele.flatten}
     end
 
     def render
