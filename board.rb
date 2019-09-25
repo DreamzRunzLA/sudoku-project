8748ea5b87e57e7c52ff87c1204180df23c022e8
DreamzRunzLA <salmaan.a.qadir@gmail.com>
2019-09-24 21:44:06 -0600

    Added skeleton for board class

--- board.rb
+++ board.rb
@@ -6,7 +6,29 @@ class Board
 
     end
 
-    
+    def from_file
+        
+    end
+
+    def render
+
+    end
+
+    def solved?
+
+    end
+
+    def row_solved?
+
+    end
+
+    def col_solved?
+
+    end
+
+    def grid_solved?
+
+    end
 
 
 end
